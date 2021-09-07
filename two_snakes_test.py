adad2cf0dfbbbc27caf46107cea3946e20f16050
dhudetz <34608613+dhudetz@users.noreply.github.com>
2021-09-07 12:07:04 -0500

    back to icosahedron models
    updated to have 3D models instead of drawing lines...
    
    support for multiple snakes as well

--- two_snakes_test.py
+++ two_snakes_test.py
@@ -4,24 +4,23 @@ from direct.task import Task
 import os, sys
 from panda3d.core import LineSegs, NodePath, MeshDrawer, AntialiasAttrib
 import numpy as np
-from random import random
 
 class Snake():
     def __init__(self, start_angles=[0,0,0,0],
-    angleSpeeds=[1,1,1,1], lengths=[10,10,10], color=(1,1,1)):
-        self.color = color
+    angle_vel=[0.5324,0.33094,0.93234,0.4583240], lengths=[25,25,15]):
         self.angles = np.deg2rad(start_angles)
-        self.angleSpeeds = np.deg2rad(angleSpeeds)
+        self.angle_vel = np.deg2rad(angle_vel)
         self.l = lengths
+        self.frame_number = 0
         #self.speed = speed
 
-    def getColor(self):
-        return self.color
-
-    def nextFrame(self):
+    def next_frame(self):
         #update angles
         for t in range(len(self.angles)):
-            self.angles[t] += self.angleSpeeds[t]
+            self.angles[t] += self.angle_vel[t]
+
+        #self.node = NodePath(self.ls.create(True))
+        #self.node.setAntialias(AntialiasAttrib.MLine)
         return self.getHeadPosition()
 
     def getHeadPosition(self):
@@ -44,53 +43,39 @@ class App(ShowBase):
         ShowBase.__init__(self)
 
         #Set properties
-        self.numSnakes = 2
-        angleRange = (0, 360)
-        speedRange = (0.1, 0.2)
-        lengthRange = (25,30)
-        self.modelSize = 4
+
 
     	#Set background color of scene (dark purple)
-        base.setBackgroundColor(0,0,0)
+        base.setBackgroundColor(1,1,1)
         self.taskMgr.add(self.spinCameraTask, "SpinCameraTask")
         self.taskMgr.add(self.updateSnakes, "UpdateSnakes")
         self.nodes = []
-        self.numNodes = 300
+        self.numNodes = 100
         self.snakes = []
-        for i in range(self.numSnakes):
-            startAngles=[]
-            startSpeeds=[]
-            startLengths=[]
-            for j in range(4):
-                startAngles.append(random()*(angleRange[1]-angleRange[0])+angleRange[0])
-            for j in range(4):
-                startSpeeds.append(random()*(speedRange[1]-speedRange[0])+speedRange[0])
-            for j in range(3):
-                startLengths.append(random()*(lengthRange[1]-lengthRange[0])+lengthRange[0])
-            color = (random(), random(), random())
-            self.snakes.append(Snake(startAngles,startSpeeds,startLengths,color))
+        self.snakes.append(Snake())
+        self.snakes.append(Snake())
         self.cameraSpinSpeed = 0
         self.currentNode = None
-        self.frameNumber = 0
 
     def updateSnakes(self, task):
-        self.frameNumber += 1
-        base.movie(namePrefix=str(self.frameNumber), duration=1, fps=1, format='png')
         for s in range(len(self.snakes)):
             if len(self.nodes)>self.numNodes-1:
                 self.nodes[0].removeNode()
                 self.nodes.pop(0)
-            new_position = self.snakes[s].nextFrame()
+            new_position = self.snakes[s].next_frame()
             new_node = loader.loadModel("\\models\\Icosahedron.egg")
-            #new_position = (-new_position[0], -new_position[1], -new_position[2])
+            if s == 1:
+                new_position = (-new_position[0], -new_position[1], -new_position[2])
             new_node.setPos(new_position)
-            color = self.snakes[s].getColor()
-            new_node.setColor(color[0],color[1],color[2])
+            if s == 0:
+                new_node.setColor(1,0,0)
+            else:
+                new_node.setColor(0,0,1)
             new_node.setTransparency(True)
             new_node.reparentTo(self.render)
             self.nodes.append(new_node)
             for i in range(len(self.nodes)):
-                self.nodes[i].setScale(self.modelSize*i/len(self.nodes),self.modelSize*i/len(self.nodes),self.modelSize*i/len(self.nodes))
+                self.nodes[i].setScale(i/len(self.nodes),i/len(self.nodes),i/len(self.nodes))
                 self.nodes[i].setAlphaScale(i/len(self.nodes))
         return Task.cont
 
